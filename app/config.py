0903bb89d9636ac2fbd57b898baa8d14ed63a9d0
Aman Sharma <50880071+code-by-aman@users.noreply.github.com>
2025-03-10 20:44:40 +0530

    added port

--- app/config.py
+++ app/config.py
@@ -2,6 +2,7 @@ import os
 
 class Settings:
     SECRET_KEY = os.environ.get("SECRET_KEY", "1mpca$(2)")
+    PORT = int(os.getenv("PORT", 4000))
     ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES = 30
     REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # 7 days
